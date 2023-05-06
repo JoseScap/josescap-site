e42fc9b8a0977645078aecdcda8dd866d11ea67b
Juan Jose Puente Scapolatempo <juanpuentescapolatempo@gmail.com>
2023-05-06 16:39:41 -0300

    changing styles

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -16,33 +16,29 @@ interface CardProps {
 }
 
 export default function Card({ id, name, description, technologies, mainImage: { url } }: CardProps) {
-  return <div className="card w-full max-w-sm mx-auto bg-base-100 shadow-xl shadow-neutral-900 hover:shadow-neutral-700">
-    <figure className="px-10 pt-10">
-      <Image src={url} width={200} height={200} alt="Shoes" className="rounded-xl w-full" />
+  return <div className={$card}>
+    <figure className={$cardImageWrapper}>
+      <Image src={url} width={200} height={200} alt="Shoes" className={$cardImage} />
     </figure>
-    <div className="card-body text-center items-center">
-      <h2 className="card-title">{name}</h2>
-      <p className="line-clamp-3 mb-2">{description}</p>
-
-      
-
-      <div className="card-badges">
+    <div className={$cardBody}>
+      <h2 className={$cardTitle}>{name}</h2>
+      <p className={$cardDescription}>{description}</p>
+      <div className={$cardBadgesWrapper}>
         {
           technologies.slice(0, 5).map(({ technology }, idx) => (
-            <div className="badge badge-outline badge-accent mx-2" key={idx}>{technology.name}</div>
+            <div className={$cardBadge} key={idx}>{technology.name}</div>
           ))
         }
         {
           technologies.length > 5 && (
-            <div className="badge badge-outline badge-accent mx-2">...</div>
+            <div className={$cardBadge}>...</div>
           )
         }
       </div>
-
-      <div className="card-actions mt-4 w-full">
+      <div className={$cardActions}>
         <Link
           href={`/projects/${id}`}
-          className="btn btn-info btn-outline w-3/4 mx-auto"
+          className={$cardLink}
         >
           Ver más
         </Link>
@@ -50,3 +46,21 @@ export default function Card({ id, name, description, technologies, mainImage: {
     </div>
   </div>
 }
+
+const $card = 'card w-full max-w-sm mx-auto bg-base-100 shadow-xl shadow-neutral-900 hover:shadow-neutral-700'
+
+const $cardImageWrapper = 'px-10 pt-10'
+const $cardImage = 'rounded-xl w-full'
+
+const $cardBody = 'card-body text-center items-center'
+
+const $cardTitle = 'card-title'
+
+const $cardDescription = 'line-clamp-3 mb-2'
+
+const $cardBadgesWrapper = 'card-badges'
+const $cardBadge = 'badge badge-outline badge-accent mx-2'
+
+const $cardActions = 'card-actions mt-4 w-full'
+
+const $cardLink = 'btn btn-info btn-outline w-3/4 mx-auto'
